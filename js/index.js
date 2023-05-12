5572bd5210c977aa5c3411d5f4a7c8fbb24f8b16
cristian <ch374185@gmail.com>
2023-05-12 16:25:10 -0600

    agregamos gitignore

---
+++ js/index.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Personal</title>
+</head>
+<body>
+    <h1>Bienvenido a mi Págian personal</h1>
+    <hr>
+    <p>Hola Mi Nombre es cristian </p>
+    <button onclick="saludo()" >Saludar</button>
+    <script src="./js/app.js"></script>
+</body>
+</html>
+
